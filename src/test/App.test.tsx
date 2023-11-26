675b542594c5cf493c2ec5ab4c713a5ee0a81838
David Sarmiento <edst83@gmail.com>
2023-11-26 15:09:46 -0600

    update tests

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -3,6 +3,13 @@ import App from '../App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/reload/i);
-  expect(linkElement).toBeInTheDocument();
+  const btnSubmit = screen.getByText(/Registrar/i);
+  expect(btnSubmit).toBeInTheDocument();
+  expect(btnSubmit).toBeDisabled
+});
+
+test('input textfield is empty', () => {
+  const result = render(<App />);
+  const element = result.container.querySelector('#numeric_input');
+  expect(element?.textContent).toEqual('');
 });
